f2eefd838d2f05bd1e2f97d52c1c47b287556a40
Mike Whiting <mail@mikejw.co.uk>
2025-06-07 08:26:04 +0100

    ansible fix

--- nix/start.sh
+++ nix/start.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 
+export LANGUAGE="C.UTF-8"
+export LANG="C.UTF-8"
+export LC_COLLATE="C.UTF-8"
+export LC_CTYPE="C.UTF-8"
+export LC_MONETARY="C.UTF-8"
+export LC_NUMERIC="C.UTF-8"
+export LC_TIME="C.UTF-8"
+export LC_MESSAGES="C.UTF-8"
+export LC_ALL="C.UTF-8"
+
+
 XDG_CONFIG_HOME=./.config $(which fish)
 
 
