d0a606ff1deb8eb3c45806e80ac5854ad7ce893c
Mike Whiting <mail@mikejw.co.uk>
2025-06-07 07:27:58 +0100

    add files

---
+++ .config/fish/config.fish
@@ -0,0 +1,35 @@
+
+function motd
+    echo -n "
+  _.........._
+ | |dotfiles| |
+ | |        | |
+ | |        | |
+ | |________| |
+ |   ______   |
+ |  |    | |  |
+ |__|____|_|__|
+"
+end
+
+function start_tmux
+    if type -q tmux
+        if not test -n "$TMUX"
+            tmux attach-session -t default; or tmux new-session -s default
+            alias exit "exit_nix"
+
+        else
+            fish_config theme choose "Base16 Eighties"
+            motd
+            nvm install v22.16.0
+            fish_add_path ./bin
+
+            alias exit "exit"
+        end
+    end
+end
+
+if status is-interactive
+    set SHELL (which fish)
+    start_tmux
+end
