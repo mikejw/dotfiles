2874de3d2a795f1e6e424b934a9c5a1aa35c9d06
Mike Whiting <mail@mikejw.co.uk>
2025-12-15 19:43:54 +0000

    add composer

---
+++ .config/fish/functions/composer.fish
@@ -0,0 +1,3 @@
+function composer --wraps='php ~/code/dotfiles/bin/composer.phar' --description 'alias composer=php ~/code/dotfiles/bin/composer.phar'
+    php ~/code/dotfiles/bin/composer.phar $argv
+end
