1e7c1572b9384d9d49b487a108bc844ca7961739
Mike Whiting <mail@mikejw.co.uk>
2025-12-21 05:56:44 +0000

    make path more robust

--- nix/start.sh
+++ nix/start.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-XDG_CONFIG_HOME=./.config $(which fish)
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+
+export XDG_CONFIG_HOME="$PROJECT_ROOT/.config"
+exec $(which fish)
