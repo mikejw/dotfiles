734f15d15df86cd281dd80bb3cf9743ff8f98afa
Mike Whiting <mail@mikejw.co.uk>
2026-01-05 20:04:11 +0000

    cortex cli location

--- .config/fish/functions/cortex.fish
+++ .config/fish/functions/cortex.fish
@@ -1,3 +1,7 @@
-function cortex --wraps='php ~/code/dotfiles/bin/cortex.phar' --description 'alias cortex=php ~/code/dotfiles/bin/cortex.phar'
-    php ~/code/dotfiles/bin/cortex.phar $argv
+#function cortex --wraps='php ~/code/dotfiles/bin/cortex.phar' --description 'alias cortex=php ~/code/dotfiles/bin/cortex.phar'
+#    php ~/code/dotfiles/bin/cortex.phar $argv
+#end
+
+function cortex --wraps='php ~/code/gigabyte/cortex-cli/cortex.phar' --description 'alias cortex=php ~/code/dotfiles/bin/cortex.phar'
+    php ~/code/gigabyte/cortex-cli/cortex.phar $argv
 end
